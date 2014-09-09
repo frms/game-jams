935febdcfb8e82c94338579855417edc9cf25922
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-09-09 14:44:20 -0400

    Made the camera follow the player. Also fixed a minor movement bug when the player changed orientation

--- chain-reaction/Assets/Scripts/Player.cs
+++ chain-reaction/Assets/Scripts/Player.cs
@@ -28,11 +28,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 stickDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		//Debug.Log (stickDirection);
-		
+
+		stickDirection = stickDirection.normalized;
+
 		if(stickDirection.sqrMagnitude > 0.031) {
-			// Move the character
-			rigidbody2D.velocity = transform.right * speed;
 
 			// Rotate the character
 			float moveAngle = Mathf.Atan2(stickDirection.y, stickDirection.x)*Mathf.Rad2Deg;
@@ -41,10 +40,18 @@ public class Player : MonoBehaviour {
 			}
 			
 			//Debug.Log (moveAngle);
-			
-			float rotation = moveAngle;
-			//float rotation = getDirection(moveAngle);
-			transform.rotation = Quaternion.Euler(0, 0, rotation);
+
+			// If the orientation of the character has changed then update it, but don't move the character yet
+			// because Unity has a minor bug where sometimes when you move and change orientation at the same
+			// time you move partly in a direction you dont want to go.
+			if(transform.rotation != Quaternion.Euler(0, 0, moveAngle)) {
+				transform.rotation = Quaternion.Euler(0, 0, moveAngle);
+			}
+			// Else we are facing the right orientation so move the character
+			else {
+				rigidbody2D.velocity = transform.right * speed;
+			}
+
 		} else {
 			rigidbody2D.velocity = Vector2.zero;
 		}
@@ -54,37 +61,6 @@ public class Player : MonoBehaviour {
 		if (Input.GetButtonDown ("Fire2")) {
 			fireArrow(maxArrowDistance);
 		}
-	}
-	
-	private float getDirection(float moveAngle) {
-		if(moveAngle < 22.5 || moveAngle >= 337.5) {
-			// Face right
-			return 0;
-		} else if(moveAngle < 67.5 && moveAngle >= 22.5) {
-			// Face up right
-			return 45;
-		} else if(moveAngle < 112.5 && moveAngle >= 67.5) {
-			// Face up
-			return 90;
-		} else if(moveAngle < 157.5 && moveAngle >= 112.5) {
-			// Face up left
-			return 135;
-		} else if(moveAngle < 202.5 && moveAngle >= 157.5) {
-			// Face left
-			return 180;
-		} else if(moveAngle < 247.5 && moveAngle >= 202.5) {
-			// Face down left
-			return 225;
-		} else if(moveAngle < 292.5 && moveAngle >= 247.5) {
-			// Face down
-			return 270;
-		} else if(moveAngle < 337.5 || moveAngle >= 292.5) {
-			// Face down right
-			return 315;
-		}
-		
-		Debug.Log ("SHOULD NOT BE REACHING THIS CODE");
-		return 315;
 	}
 
 	private void updateMeleeAttack() {
