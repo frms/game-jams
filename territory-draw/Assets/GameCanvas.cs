d045c4d0070a5d8a215ca56cadf5fa438dbbe154
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-09-20 14:47:40 -0400

    Adding territory draw prototype

---
+++ territory-draw/Assets/GameCanvas.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCanvas : MonoBehaviour {
+	public int x = 0;
+	public int y = 0;
+	
+	private Texture2D tex;
+	
+	void Start(){
+		tex = (Texture2D) renderer.material.mainTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Color myPixel = tex.GetPixel(x,y);
+//		
+//		Debug.Log ( myTexture.width + " " + myTexture.height );
+//		
+//		if(MyPixel == Color.red) {
+//			Debug.Log ("Red");
+//		} else if(MyPixel == Color.green) {
+//			Debug.Log ("Green");
+//		} else if(MyPixel == Color.blue) {
+//			Debug.Log ("Blue");
+//		} else if(MyPixel == Color.white) {
+//			Debug.Log("White");
+//		}
+
+		clickPixel();
+	}
+
+	private void clickPixel ()
+	{
+		if (!Input.GetMouseButton (0))
+			return;
+
+		RaycastHit hit;
+		if (!Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit))
+			return;
+		
+		Renderer renderer = hit.collider.renderer;
+		MeshCollider meshCollider = hit.collider as MeshCollider;
+		if (renderer == null || renderer.sharedMaterial == null || renderer.sharedMaterial.mainTexture == null || meshCollider == null)
+			return;
+
+		Vector2 pixelUV = hit.textureCoord;
+		print ((int)(pixelUV.x * tex.width) + "--" + (int)(pixelUV.y * tex.height));
+
+		tex.SetPixel((int) (pixelUV.x * tex.width), (int) (pixelUV.y * tex.height), Color.red);
+	}
+}
---
+++ territory-draw/Assets/TestTexture.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTexture : MonoBehaviour {
+	public int x = 0;
+	public int y = 0;
+
+	private Texture2D myTexture;
+	private Sprite mySprite;
+	
+	void Start(){
+		mySprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+		myTexture = mySprite.texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color MyPixel = myTexture.GetPixel(x,y);
+
+		Debug.Log ( myTexture.width + " " + myTexture.height );
+
+		if(MyPixel == Color.red) {
+			Debug.Log ("Red");
+		} else if(MyPixel == Color.green) {
+			Debug.Log ("Green");
+		} else if(MyPixel == Color.blue) {
+			Debug.Log ("Blue");
+		}
+	}
+}
