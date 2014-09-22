6c2512d00aae7d0aaa8fbb9266d9a33e83e9150a
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-09-22 08:54:45 -0400

    Removed the movement bug fix that I used to have for the player suddenly chagning direction. I think the latest version of unity must have fixed it. Also made the player move in all directions (rather than just 8) with an analog stick.

--- chain-reaction/Assets/Scripts/Player.cs
+++ chain-reaction/Assets/Scripts/Player.cs
@@ -89,53 +89,12 @@ public class Player : MonoBehaviour {
 			}
 			
 			//Debug.Log (moveAngle);
-			moveAngle = getDirection(moveAngle);
 
-			// If the orientation of the character has changed then update it, but don't move the character yet
-			// because Unity has a minor bug where sometimes when you move and change orientation at the same
-			// time you move partly in a direction you dont want to go.
-			if(transform.rotation != Quaternion.Euler(0, 0, moveAngle)) {
-				transform.rotation = Quaternion.Euler(0, 0, moveAngle);
-			}
-			// Else we are facing the right orientation so move the character
-			else {
-				rigidbody2D.velocity = transform.right * speed;
-			}
-			
+			transform.rotation = Quaternion.Euler(0, 0, moveAngle);
+			rigidbody2D.velocity = transform.right * speed;
 		} else {
 			rigidbody2D.velocity = Vector2.zero;
 		}
-	}
-
-	private float getDirection(float moveAngle) {
-		if(moveAngle < 22.5 || moveAngle >= 337.5) {
-			// Face right
-			return 0;
-		} else if(moveAngle < 67.5 && moveAngle >= 22.5) {
-			// Face up right
-			return 45;
-		} else if(moveAngle < 112.5 && moveAngle >= 67.5) {
-			// Face up
-			return 90;
-		} else if(moveAngle < 157.5 && moveAngle >= 112.5) {
-			// Face up left
-			return 135;
-		} else if(moveAngle < 202.5 && moveAngle >= 157.5) {
-			// Face left
-			return 180;
-		} else if(moveAngle < 247.5 && moveAngle >= 202.5) {
-			// Face down left
-			return 225;
-		} else if(moveAngle < 292.5 && moveAngle >= 247.5) {
-			// Face down
-			return 270;
-		} else if(moveAngle < 337.5 || moveAngle >= 292.5) {
-			// Face down right
-			return 315;
-		}
-		
-		Debug.Log ("SHOULD NOT BE REACHING THIS CODE");
-		return 315;
 	}
 
 	private void updateMeleeAttack() {
