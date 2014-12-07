50effe744c41942c7fc625aa71f9199b5f6ba4cd
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-12-06 21:55:07 -0500

    Made a game controller that spawns enemies in random places.

---
+++ ludum-dare-31/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject enemy1;
+	public int numOfEnemy = 5;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < numOfEnemy; i++) {
+			float x = Random.value * 16 - 8;
+			float y = Random.value * 9 - 4.5f;
+
+			Quaternion orientation = Quaternion.Euler(0, 0, Random.value * 360);
+
+			Instantiate (enemy1, new Vector3(x, y, 0), orientation);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
