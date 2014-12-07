b4fb51e4d6b4171427ebe37a475e79eae5dc256a
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-12-07 00:44:15 -0500

    Made a restart button appear on player death.

--- ludum-dare-31/Assets/Scripts/GameController.cs
+++ ludum-dare-31/Assets/Scripts/GameController.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+	public Button restartBtn;
 
 	public GameObject enemy1;
 	public int numOfEnemy = 5;
 
+	private GameObject player;
+
 	// Use this for initialization
 	void Start () {
+		player = GameObject.Find("Player");
+
 		for(int i = 0; i < numOfEnemy; i++) {
 			float x = Random.value * 16 - 8;
 			float y = Random.value * 9 - 4.5f;
@@ -20,6 +26,12 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(player == null) {
+			restartBtn.gameObject.SetActive(true);
+		}
+	}
+
+	public void restartGame() {
+		Application.LoadLevel(0);
 	}
 }
