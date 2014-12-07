9c3f5a54146e3502f1ea555a0453ba7183252a84
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-12-06 21:42:48 -0500

    Made the enemy wander around the level boundary and move away from the wall when it hits it.

--- ludum-dare-31/Assets/Scripts/SteeringUtils.cs
+++ ludum-dare-31/Assets/Scripts/SteeringUtils.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 /* A helper class for steering a game object in 2D */
 public class SteeringUtils : MonoBehaviour {
-
+	
 	public float maxVelocity = 3;
-
+	
 	/* The maximum acceleration */
 	public float maxAcceleration = 4;
-
+	
 	/* Updates the velocity of the current game object by the given linear acceleration */
 	public void steer(Vector2 linearAcceleration) {
 		rigidbody2D.velocity += linearAcceleration * Time.fixedDeltaTime;
@@ -17,28 +17,28 @@ public class SteeringUtils : MonoBehaviour {
 			rigidbody2D.velocity = rigidbody2D.velocity.normalized * maxVelocity;
 		}
 	}
-
+	
 	/* Calls the normal Vector2 linear acceleration */
 	public void steer(Vector3 linearAcceleration) {
 		this.steer (new Vector2 (linearAcceleration.x, linearAcceleration.y));
 	}
-
+	
 	/* A seek steering behavior. Will return the steering for the current game object to seek a given position */
 	public Vector2 seek(Vector3 targetPosition) {
 		//Get the direction
 		Vector3 acceleration = targetPosition - transform.position;
-
+		
 		//Remove the z coordinate
 		acceleration.z = 0;
-
+		
 		acceleration.Normalize ();
-
+		
 		//Accelerate to the target
 		acceleration *= maxAcceleration;
-
+		
 		return acceleration;
 	}
-
+	
 	/* Makes the current game object look where he is going */
 	public void lookWhereYoureGoing() {
 		Vector2 direction = rigidbody2D.velocity.normalized;
@@ -46,7 +46,7 @@ public class SteeringUtils : MonoBehaviour {
 		// If we have a non-zero velocity then look towards where we are moving otherwise do nothing
 		if (rigidbody2D.velocity.sqrMagnitude > 0.001) {
 			float toRotation = (Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg);
-			float rotation = Mathf.LerpAngle(transform.rotation.eulerAngles.z, toRotation, Time.fixedDeltaTime*2);
+			float rotation = Mathf.LerpAngle(transform.rotation.eulerAngles.z, toRotation, Time.fixedDeltaTime*5);
 			
 			transform.rotation = Quaternion.Euler(0, 0, rotation);
 		}
--- ludum-dare-31/Assets/Scripts/Wander.cs
+++ ludum-dare-31/Assets/Scripts/Wander.cs
@@ -2,58 +2,101 @@
 using System.Collections;
 
 public class Wander : MonoBehaviour {
-		
+	
 	/* The forward offset of the wander square */
-	public float wanderOffset = 4;
+	public float wanderOffset = 1.5f;
 	
 	/* The radius of the wander square */
 	public float wanderRadius = 4;
 	
 	/* The rate at which the wander orientation can change */
 	public float wanderRate = 0.4f;
-
+	
 	private float wanderOrientation = 0;
-
+	
 	private GameObject debugRing;
-
+	
 	private SteeringUtils steeringUtils;
+	
+	
+	public float rayDistance = 2;
+	public float sameDecisionDuration = 3;
+	
+	private float lastDecisionTime = Mathf.NegativeInfinity;
+	private int decision;
+
+	private int levelBoundaryMask;
 
 	void Start() {
-		//DebugDraw debugDraw = gameObject.GetComponent<DebugDraw> ();
-		//debugRing = debugDraw.createRing (Vector3.zero, wanderRadius);
-
+		//		DebugDraw debugDraw = gameObject.GetComponent<DebugDraw> ();
+		//		debugRing = debugDraw.createRing (Vector3.zero, wanderRadius);
+		
 		steeringUtils = gameObject.GetComponent<SteeringUtils> ();
+
+		levelBoundaryMask = (1 << LayerMask.NameToLayer("LevelBoundary"));
 	}
-
+	
 	void FixedUpdate () {
-
+		
 		float characterOrientation = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
-
+		
 		/* Update the wander orientation */
-		wanderOrientation += randomBinomial() * wanderRate;
+		updateWanderOrientation (characterOrientation);
 		
 		/* Calculate the combined target orientation */
 		float targetOrientation = wanderOrientation + characterOrientation;
 		
 		/* Calculate the center of the wander circle */
 		Vector3 targetPosition = transform.position + (orientationToVector (characterOrientation) * wanderOffset);
-
-		//debugRing.transform.position = targetPosition;
+		
+		//		debugRing.transform.position = targetPosition;
 		
 		/* Calculate the target position */
 		targetPosition = targetPosition + (orientationToVector(targetOrientation) * wanderRadius);
-
-		//Debug.DrawLine (transform.position, targetPosition);
-
-
+		
+		//		Debug.DrawLine (transform.position, targetPosition);
+		
+		
 		Vector2 acceleration = steeringUtils.seek (targetPosition);
-
+		
 		steeringUtils.steer (acceleration);
-	
+		
 		steeringUtils.lookWhereYoureGoing ();
-
+		
 	}
-
+	
+	private void updateWanderOrientation(float characterOrientation) {
+		Vector3 dir = orientationToVector (characterOrientation);
+		
+		//		Vector3 end = transform.position + dir * rayDistance;
+		//		Debug.DrawLine (transform.position, end, Color.green);
+		
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, rayDistance, levelBoundaryMask);
+		/* If we collide with a wall then move away from it */
+		if (hit.collider != null){
+			/* If we have not already decided which direction we will move away from the 
+			 * wall then decide it now */
+			if(lastDecisionTime + sameDecisionDuration < Time.time) {
+				decision = (int)(Random.value * 2);
+				lastDecisionTime = Time.time;
+			}
+			/* Set the wander orientation to the decided direction */
+			else {
+				wanderOrientation = Mathf.PI/2;
+				if(decision == 0) {
+					wanderOrientation *= -1;
+				}
+			}
+			
+			//Debug.Log (wanderOrientation);
+			//Debug.Log (hit.transform.name);
+		}
+		/* Else update the wander orienation randomly like normal */
+		else {
+			wanderOrientation += randomBinomial() * wanderRate;
+		}
+	}
+	
 	/* Returns a random number between -1 and 1. Values around zero are more likely. */
 	float randomBinomial() {
 		return Random.value - Random.value;
@@ -63,4 +106,5 @@ public class Wander : MonoBehaviour {
 	Vector3 orientationToVector(float orientation) {
 		return new Vector3(Mathf.Cos(orientation), Mathf.Sin(orientation), 0);
 	}
+
 }
