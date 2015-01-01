e6cc2e65741b0a264df5f1ff1d5c1ee197c6b781
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-12-31 22:05:57 -0500

    be able to switch wrapper

--- cupcakes/Assets/Scripts/CreatorController.cs
+++ cupcakes/Assets/Scripts/CreatorController.cs
@@ -1,17 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CreatorController : MonoBehaviour {
+	public Text label;
 
 	public GameObject[] wrappers;
 
+	private int wrapperIndex = 0;
+
+	private GameObject wrapperShown;
+
 	// Use this for initialization
 	void Start () {
-		Instantiate (wrappers [0]);
+		showCurrentWrapper ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void next() {
+		wrapperIndex++;
+		wrapperIndex %= wrappers.Length;
+		showCurrentWrapper ();
+	}
+
+	public void prev() {
+		wrapperIndex--;
+		wrapperIndex = (wrapperIndex < 0) ? wrappers.Length - 1 : wrapperIndex;
+		showCurrentWrapper ();
+	}
+
+	private void showCurrentWrapper() {
+		Destroy (wrapperShown);
+		wrapperShown = Instantiate (wrappers [wrapperIndex]) as GameObject;
+		label.text = wrappers [wrapperIndex].name;
 	}
 }
