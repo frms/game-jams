8ba26954ce2d7533dc0c0685c477c8c9df4824a6
Wooden Rabbit <woodenrabbitfoo@gmail.com>
2014-12-31 21:41:03 -0500

    adding a game controller.

---
+++ cupcakes/Assets/Scripts/CreatorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatorController : MonoBehaviour {
+
+	public GameObject[] wrappers;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (wrappers [0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
