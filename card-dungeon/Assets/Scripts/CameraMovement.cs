b1ef47d622fd203327dc941c7ef2d70f979cfc59
Anton Pantev <pantevanton@gmail.com>
2015-03-15 22:53:49 -0400

    make the camera follow the player

---
+++ card-dungeon/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+	}
+	
+	// LateUpdate is called once per frame after the other normal Update functions have already run
+	void LateUpdate () {
+		transform.position = target.position + new Vector3 (0, 0, -10.0f);
+	}
+}
