a63683c9c8cb44f03e966e3c70cced5161677d51
Anton Pantev <pantevanton@gmail.com>
2015-03-18 14:04:24 -0400

    Tried out navmesh with a basic tile map and nav obstacles that carve for walls. Worked okish, but the generated nav mesh gets hairy since each wall tile is one obstacle so there are a lot more triangles in the nav mesh than there needs to be.

---
+++ card-dungeon/Assets/Scripts/EnemyBasic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBasic : MonoBehaviour {
+
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 dest = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			dest.y = 0;
+			agent.SetDestination (dest);
+		} else if (Input.GetMouseButtonDown (1)) {
+			agent.Stop();
+		}
+	}
+}
