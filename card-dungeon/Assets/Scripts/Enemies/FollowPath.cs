f477d20d24372642b9d448329608c31ad880f277
Anton Pantev <pantevanton@gmail.com>
2015-03-28 00:19:12 -0400

    take into account 1 node paths

--- card-dungeon/Assets/Scripts/Enemies/FollowPath.cs
+++ card-dungeon/Assets/Scripts/Enemies/FollowPath.cs
@@ -22,36 +22,44 @@ public class FollowPath : MonoBehaviour {
 	}
 
 	public Vector2 getSteering (LinePath path) {
-		
-		/* Find the final destination of the character on this path */
-		Vector2 finalDestination = (pathDirection > 0) ? path[path.Length-1] : path[0];
-		
-		/* If we are close enough to the final destination then either stop moving or reverse if 
-		 * the character is set to loop on paths */
-		if( Vector2.Distance(transform.position, finalDestination) < stopRadius ) {
-			if(pathLoop) {
-				pathDirection *= -1;
-			} else {
-				rb.velocity = Vector2.zero;
-				return Vector2.zero;
+		Vector2 targetPosition;
+
+		// If the path has only one node then just go to that position;
+		if (path.Length == 1) {
+			targetPosition = path[0];
+		}
+		// Else find the closest spot on the path to the character and go to that instead.
+		else {
+			/* Find the final destination of the character on this path */
+			Vector2 finalDestination = (pathDirection > 0) ? path[path.Length-1] : path[0];
+			
+			/* If we are close enough to the final destination then either stop moving or reverse if 
+			 * the character is set to loop on paths */
+			if( Vector2.Distance(transform.position, finalDestination) < stopRadius ) {
+				if(pathLoop) {
+					pathDirection *= -1;
+				} else {
+					rb.velocity = Vector2.zero;
+					return Vector2.zero;
+				}
 			}
+			
+			/* Get the param for the closest position point on the path given the character's position */
+			float param = path.getParam(transform.position);
+			
+			/* Move down the path */
+			param += pathDirection * pathOffset;
+			
+			/* Make sure we don't move past the beginning or end of the path */
+			if (param < 0) {
+				param = 0;
+			} else if (param > path.maxDist) {
+				param = path.maxDist;
+			}
+			
+			/* Set the target position */
+			targetPosition = path.getPosition(param);
 		}
-		
-		/* Get the param for the closest position point on the path given the character's position */
-		float param = path.getParam(transform.position);
-		
-		/* Move down the path */
-		param += pathDirection * pathOffset;
-		
-		/* Make sure we don't move past the beginning or end of the path */
-		if (param < 0) {
-			param = 0;
-		} else if (param > path.maxDist) {
-			param = path.maxDist;
-		}
-		
-		/* Set the target position */
-		Vector2 targetPosition = path.getPosition(param);
 		
 		return steeringUtils.arrive(targetPosition);
 	}
