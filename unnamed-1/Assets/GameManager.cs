3a3ecf1a03e67b677a108aea9e9b61f2ab5174a4
Anton Pantev <pantevanton@gmail.com>
2015-05-11 18:50:20 -0400

    added a player city

--- unnamed-1/Assets/GameManager.cs
+++ unnamed-1/Assets/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour {
 	public Transform city;
 	public int numCities;
 
+	public Transform playerCity;
+
 	private List<Vector3> cities;
 	private Vector3 nullPos = new Vector3 (-1000, -1000, -1000);
 
@@ -14,18 +16,24 @@ public class GameManager : MonoBehaviour {
 	void Start () {
 		cities = new List<Vector3> ();
 
+		addCity (playerCity);
+
 		for (int i = 0; i < numCities; i++) {
-			Vector3 pos;
+			addCity(city);
+		}
+	}
 
-			for(int j = 0; j < 5; j++) {
-				pos = getPos();
-
-				if(pos != nullPos) {
-					Instantiate(city, pos, Quaternion.identity);
-					cities.Add(pos);
-
-					break;
-				}
+	private void addCity(Transform c) {
+		Vector3 pos;
+		
+		for (int j = 0; j < 5; j++) {
+			pos = getPos ();
+			
+			if (pos != nullPos) {
+				Instantiate (c, pos, Quaternion.identity);
+				cities.Add (pos);
+				
+				break;
 			}
 		}
 	}
