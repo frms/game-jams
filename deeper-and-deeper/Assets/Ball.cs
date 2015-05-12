c63bfbd5473b448314b8e92c439031c99f4065af
Anton Pantev <pantevanton@gmail.com>
2015-05-12 19:43:55 -0400

    Added a ball boundary

---
+++ deeper-and-deeper/Assets/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float initialVelocity = 6f;
+	
+	
+	private Rigidbody2D rb;
+	private bool ballInPlay;
+	
+	void Awake () {
+		
+		rb = GetComponent<Rigidbody2D>();
+		ballInPlay = false;
+		
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
+			transform.parent = null;
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.velocity = new Vector2(0, -initialVelocity);
+		}
+	}
+}
---
+++ deeper-and-deeper/Assets/BallBoundary.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBoundary : MonoBehaviour {
+	private EdgeCollider2D col;
+
+	// Use this for initialization
+	void Awake () {
+		float halfY = Camera.main.orthographicSize;
+		float halfX = halfY * Camera.main.aspect;
+
+		Vector2[] points = new Vector2[5];
+
+		points [0] = new Vector2 (-halfX, -halfY);
+		points [1] = new Vector2 (halfX, -halfY);
+		points [2] = new Vector2 (halfX, halfY);
+		points [3] = new Vector2 (-halfX, halfY);
+		points [4] = new Vector2 (-halfX, -halfY);
+
+		col = GetComponent<EdgeCollider2D> ();
+
+		col.points = points;
+	}
+
+}
