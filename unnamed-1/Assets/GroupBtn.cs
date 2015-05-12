799b8061c57a5f388a0ef1e3e003cd3bee2c95c9
Anton Pantev <pantevanton@gmail.com>
2015-05-11 21:37:08 -0400

    adding a button that knows when you click on an enemy city

---
+++ unnamed-1/Assets/GroupBtn.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GroupBtn : MonoBehaviour, IDeselectHandler {
+	private Image img;
+
+	// Use this for initialization
+	void Start () {
+		img = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void click() {
+		if (img.color == Color.yellow) {
+			EventSystem.current.SetSelectedGameObject (null);
+		} else {
+			img.color = Color.yellow;
+		}
+	}
+	
+	public void OnDeselect (BaseEventData data) 
+	{
+		img.color = Color.white;
+
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+		if (Physics.Raycast (ray, out hit) && hit.transform.tag == "EnemyCity"){
+			print (hit.transform.name);
+		}
+	}
+}
