bce43156ecfda3f181de84d5ee13c618395c25ee
Anton Pantev <pantevanton@gmail.com>
2015-05-17 16:04:51 -0400

    added bricks

---
+++ deeper-and-deeper/Assets/Brick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		sr.color = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		Destroy(gameObject);
+	}
+}
