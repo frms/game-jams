4a3a018f7dcc3f7653fa16db1ef7511789326cb0
Anton Pantev <pantevanton@gmail.com>
2015-05-22 16:10:31 -0400

    changing background color

--- deeper-and-deeper/Assets/CameraMovement.cs
+++ deeper-and-deeper/Assets/CameraMovement.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class CameraMovement : MonoBehaviour {
 	public float speed = 6;
 
+	public float colorSpeed = 0.005f;
+
+	private Camera cam;
+
+	void Start() {
+		cam = GetComponent<Camera> ();
+	}
+
 	void Update () {
 		if (Input.GetButton ("Jump")) {
 			transform.position += Vector3.up * speed * Time.deltaTime;
+
+			float h, s, v;
+			
+			EditorGUIUtility.RGBToHSV(cam.backgroundColor, out h, out s, out v);
+			
+			h += colorSpeed * Time.deltaTime;
+			
+			if (h > 1) {
+				h -= 1;
+			}
+			
+			cam.backgroundColor = EditorGUIUtility.HSVToRGB (h, s, v);
 		}
 	}
 }
