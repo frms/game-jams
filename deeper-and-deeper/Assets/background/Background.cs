65c39fc08cd0d87fd8e430f199f5286c6d476ed9
Anton Pantev <pantevanton@gmail.com>
2015-05-22 14:54:08 -0400

    cleaning up the background code

--- deeper-and-deeper/Assets/background/Background.cs
+++ deeper-and-deeper/Assets/background/Background.cs
@@ -47,20 +47,19 @@ public class Background : MonoBehaviour {
 	private int lastSectionBuilt;
 
 	public void buildSection(int startY) {
-		List<Room> rooms = buildMapData (startY);
-
 		Transform parent = (new GameObject("bgSection")).transform;
-		
-		for(int i = 0; i < rooms.Count; i++) {
-			Room r = rooms[i];
+
+		for (int i = 0; i < numberOfRooms; i++) {
+			int roomWidth = Random.Range(roomWidthRange[0], roomWidthRange[1]);
+			int roomHeight = Random.Range(roomHeightRange[0], roomHeightRange[1]);
 			
-			float x = ( r.centerX - ((float)(mapWidth))/2) * tileSize;
-			float y = ( r.centerY - ((float)(mapHeight))/2) * tileSize;
-			Vector3 pos = new Vector3(x, y, 0);
+			/* Add 1 because Random.Range() for ints excludes the max value */
+			int roomX = Random.Range(0, mapWidth - roomWidth + 1);
+			int roomY = Random.Range(startY, startY + mapHeight + 1);
 			
-			Transform clone = Instantiate(backgroundRect, pos, Quaternion.identity) as Transform;
-			clone.localScale = new Vector3(r.width * tileSize, r.height * tileSize, 1);
-			clone.parent = parent;
+			Room r = new Room (roomX, roomY, roomWidth, roomHeight);
+			
+			createRect(r, parent);
 		}
 
 		lastSectionBuilt = startY;
@@ -68,35 +67,13 @@ public class Background : MonoBehaviour {
 		sections.Add (parent);
 	}
 
-	private List<Room> buildMapData(int startY) {
-		List<Room> rooms = new List<Room>();
+	private void createRect(Room r, Transform parent) {
+		float x = ( r.centerX - ((float)(mapWidth))/2) * tileSize;
+		float y = ( r.centerY - ((float)(mapHeight))/2) * tileSize;
+		Vector3 pos = new Vector3(x, y, 0);
 		
-		for (int i = 0; i < numberOfRooms; i++) {
-			int roomWidth = Random.Range(roomWidthRange[0], roomWidthRange[1]);
-			int roomHeight = Random.Range(roomHeightRange[0], roomHeightRange[1]);
-
-			/* Add 1 because Random.Range() for ints excludes the max value */
-			int roomX = Random.Range(0, mapWidth - roomWidth + 1);
-			int roomY = Random.Range(startY, startY + mapHeight + 1);
-			
-			Room r = new Room (roomX, roomY, roomWidth, roomHeight);
-			
-			if(overlappingRooms || !roomCollides(r, rooms)) {
-				rooms.Add (r);
-			}
-		}
-
-		return rooms;
+		Transform clone = Instantiate(backgroundRect, pos, Quaternion.identity) as Transform;
+		clone.localScale = new Vector3(r.width * tileSize, r.height * tileSize, 1);
+		clone.parent = parent;
 	}
-	
-	public bool roomCollides(Room r, List<Room> rooms) {
-		foreach (Room r2 in rooms) {
-			if(r.collidesWith(r2)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
 }
