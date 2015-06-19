ee64897bf61bd40bc55dee1a28f67298f02d6a93
Anton Pantev <pantevanton@gmail.com>
2015-06-19 17:34:00 -0400

    starting work on shooting the arrows with the mouse position in mind

--- harvest/Assets/Scripts/Player.cs
+++ harvest/Assets/Scripts/Player.cs
@@ -8,21 +8,38 @@ public class Player : MonoBehaviour {
 	public Transform spawnPoint;
 
 	private Rigidbody rb;
+	private int groundLayer;
 
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+
+		groundLayer = 1 << LayerMask.NameToLayer ("Ground");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// A Button
-		if (Input.GetButtonDown ("Fire1")) {
-			Transform clone = Instantiate(arrow, spawnPoint.position, transform.rotation) as Transform;
+		Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			Rigidbody cloneRb = clone.GetComponent<Rigidbody>();
-			cloneRb.velocity = clone.right * speed;
+		RaycastHit hit;
+		if (Physics.Raycast (r, out hit, Mathf.Infinity, groundLayer)) {
+			//Debug.Log(hit.point);
+
+			Vector3 dir = hit.point - transform.position;
+			dir.Normalize();
+
+			float angle = limitAngle(Mathf.Atan2(dir.z, dir.x)*Mathf.Rad2Deg);
+
+			Debug.Log (angle);
+
+			// A Button
+			if (Input.GetButtonDown ("Fire1")) {
+				Transform clone = Instantiate(arrow, spawnPoint.position, transform.rotation) as Transform;
+				
+				Rigidbody cloneRb = clone.GetComponent<Rigidbody>();
+				cloneRb.velocity = clone.right * speed;
+			}
 		}
 	}
 
@@ -34,54 +51,55 @@ public class Player : MonoBehaviour {
 		Vector2 stickDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		
 		if(stickDirection.sqrMagnitude > 0.031) {
-			
 			// Rotate the character
 			float moveAngle = Mathf.Atan2(stickDirection.y, stickDirection.x)*Mathf.Rad2Deg;
-			if(moveAngle < 0) {
-				moveAngle += 360;
-			}
 
-			moveAngle = getDirection(moveAngle);
+			moveAngle = limitAngle(moveAngle);
+
+			/* The above direction value was based on a counterclockwise unit circle.
+			But we want to rotate clockwise in this case. */
+			moveAngle = 360 - moveAngle;
 			
 			transform.rotation = Quaternion.Euler(0, moveAngle, 0);
+
 			rb.velocity = transform.right * speed;
 		} else {
 			rb.velocity = Vector3.zero;
 		}
 	}
-	
-	private float getDirection(float moveAngle) {
+
+	private float limitAngle(float angle) {
+		if(angle < 0) {
+			angle += 360;
+		}
+		
 		float dir = 315;
 
-		if(moveAngle < 22.5 || moveAngle >= 337.5) {
+		if(angle < 22.5 || angle >= 337.5) {
 			// Face right
 			dir = 0;
-		} else if(moveAngle < 67.5 && moveAngle >= 22.5) {
+		} else if(angle < 67.5 && angle >= 22.5) {
 			// Face up right
 			dir = 45;
-		} else if(moveAngle < 112.5 && moveAngle >= 67.5) {
+		} else if(angle < 112.5 && angle >= 67.5) {
 			// Face up
 			dir = 90;
-		} else if(moveAngle < 157.5 && moveAngle >= 112.5) {
+		} else if(angle < 157.5 && angle >= 112.5) {
 			// Face up left
 			dir = 135;
-		} else if(moveAngle < 202.5 && moveAngle >= 157.5) {
+		} else if(angle < 202.5 && angle >= 157.5) {
 			// Face left
 			dir = 180;
-		} else if(moveAngle < 247.5 && moveAngle >= 202.5) {
+		} else if(angle < 247.5 && angle >= 202.5) {
 			// Face down left
 			dir = 225;
-		} else if(moveAngle < 292.5 && moveAngle >= 247.5) {
+		} else if(angle < 292.5 && angle >= 247.5) {
 			// Face down
 			dir = 270;
-		} else if(moveAngle < 337.5 || moveAngle >= 292.5) {
+		} else if(angle < 337.5 || angle >= 292.5) {
 			// Face down right
 			dir = 315;
 		}
-
-		/* The above direction value was based on a counterclockwise unit circle.
-		   But we want to rotate clockwise in this case. */
-		dir = 360 - dir;
 
 		return dir;
 	}
