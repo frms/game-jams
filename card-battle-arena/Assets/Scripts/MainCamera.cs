54c7338644252817b5557122480a723c6607d1fb
Anton Pantev <pantevanton@gmail.com>
2015-06-23 15:05:16 -0400

    Move the camera so the whole map is with in view when the scene starts

---
+++ card-battle-arena/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TileMap tm = GameObject.Find ("TileMap").GetComponent<TileMap> ();
+
+		float w = tm.mapWidth * tm.tileSize;
+		float h = tm.mapHeight * tm.tileSize;
+
+		Camera camera = GetComponent<Camera> ();
+
+		if (w / h < camera.aspect) {
+			camera.orthographicSize = h / 2f;
+		} else {
+			camera.orthographicSize = w / (2f * camera.aspect);
+		}
+
+		Vector3 tmPos = tm.transform.position;
+
+		Vector3 camPos = new Vector3 (tmPos.x, tmPos.y, transform.position.z);
+		camPos.x += w / 2f;
+		camPos.y += h / 2f;
+
+		transform.position = camPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
