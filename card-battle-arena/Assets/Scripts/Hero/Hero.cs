38d6bd2978160c6dda11ca09a6b481ee78f8b5a3
Anton Pantev <pantevanton@gmail.com>
2015-07-05 22:28:21 -0400

    added basic movement to the hero!

--- card-battle-arena/Assets/Scripts/Hero/Hero.cs
+++ card-battle-arena/Assets/Scripts/Hero/Hero.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class Hero : MonoBehaviour {
 	private MapData map;
 
+	private SteeringUtils steeringUtils;
+	private FollowPath followPath;
+	private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
 		map = GameObject.Find ("TileMap").GetComponent<TileMap> ().mapBuilder.map;
 
+		steeringUtils = GetComponent<SteeringUtils> ();
+		followPath = GetComponent<FollowPath> ();
+		rb = GetComponent<Rigidbody2D> ();
+
 		Debug.Log (map.worldToMapPoint(transform.position)[0] + " " + map.worldToMapPoint(transform.position)[1]);
 	}
-	
+
+	private LinePath currentPath;
+
 	// Update is called once per frame
 	void Update () {
 		// Right Click
@@ -18,11 +28,20 @@ public class Hero : MonoBehaviour {
 			int[] start = map.worldToMapPoint(transform.position);
 			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			int[] end = map.worldToMapPoint(worldPoint);
-
-			Debug.Log ("HERE!");
 			
-			LinePath currentPath = AStar.findPath (map, start, end);
+			currentPath = AStar.findPath (map, start, end);
+		}
+
+		if(currentPath != null) {
+			Vector2 accel = followPath.getSteering (currentPath);
+			steeringUtils.steer (accel);
+			steeringUtils.lookWhereYoureGoing ();
+			
 			currentPath.draw();
+		}
+		// If we have not path to the player then stand still
+		else {
+			rb.velocity = Vector2.zero;
 		}
 	}
 }
