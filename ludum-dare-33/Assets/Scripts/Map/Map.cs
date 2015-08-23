29cca858db46cb81d2a7d303bb654dfeec1dc252
Anton Pantev <pantevanton@gmail.com>
2015-08-23 12:39:04 -0400

    starting to add a random map

--- ludum-dare-33/Assets/Scripts/Map/Map.cs
+++ ludum-dare-33/Assets/Scripts/Map/Map.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Map : MonoBehaviour {
 	public int mapWidth = 30;
 	public int mapHeight = 30;
 	public float tileSize = 1f;
+
+	public int numberOfRooms = 20;
+	public bool overlappingRooms = false;
+	public bool mirrorMap = false;
+	public int[] roomWidthRange = new [] {4, 8};
+	public int[] roomHeightRange = new [] {4, 8};
+	public int[] innerHallwayWidthRange = new [] {1, 1};
+
+	private List<Room> rooms;
 
 	public Transform[] tiles;
 
@@ -15,34 +25,44 @@ public class Map : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-		map = new MapData (mapWidth, mapHeight, tileSize);
+		//buildManualMap();
 
+		build();
+
+		for(int x = 0; x < map.width; x++) {
+			for(int y = 0; y < map.height; y++) {
+				int i = map.tiles[x,y];
+
+				if(i != 0) {
+					placeThing(x, y, tiles[ i ]);
+				}
+			}
+		}
+
+		centerCamera();
+	}
+
+	public void buildManualMap() {
+		map = new MapData (mapWidth, mapHeight, tileSize);
+		
 		for(int x = 0; x < map.width; x++) {
 			for(int y = 0; y < map.height; y++) {
 				map.tiles[x,y] = 1;
 			}
 		}
-
+		
 		map.tiles[(map.width / 2) - 5, map.height / 2 + 3] = 2;
-
-		for(int x = 0; x < map.width; x++) {
-			for(int y = 0; y < map.height; y++) {
-				placeThing(x, y, tiles[ map.tiles[x,y] ]);
-			}
-		}
-
+		
 		player.position = map.mapToWorldPoint(map.width / 2, map.height / 2);
-
+		
 		Transform enemy1 = placeThing((map.width / 2) + 5, map.height / 2, enemyTypes[0]);
 		Enemy1 e1 = enemy1.GetComponent<Enemy1>();
 		e1.isLooping = true;
 		e1.currentPath = AStar.findPath(map, enemy1.position, enemy1.position + Vector3.up*map.tileSize*7, null, 0);
-
+		
 		placeThing((map.width / 2) - 4, map.height / 2 - 1, enemyTypes[0]);
-
+		
 		placeThing((map.width / 2) - 6, map.height / 2 + 3, enemyTypes[1]);
-
-		centerCamera();
 	}
 
 	private Transform placeThing (int x, int y, Transform orig) {
@@ -75,8 +95,235 @@ public class Map : MonoBehaviour {
 		Camera.main.transform.position = camPos;
 	}
 
-	// Update is called once per frame
-	void Update () {
+	public MapData build() {
+		map = new MapData (mapWidth, mapHeight, tileSize);
+		
+		rooms = new List<Room>();
+		
+		// Randomly create the rooms
+		for (int i = 0; i < numberOfRooms; i++) {
+			/* Add 1 because Random.Range() for ints excludes the max value */
+			int roomWidth = Random.Range(roomWidthRange[0], roomWidthRange[1] + 1);
+			int roomHeight = Random.Range(roomHeightRange[0], roomHeightRange[1] + 1);
+			
+			int roomX;
+			if(mirrorMap) {
+				roomX = Random.Range(0, (map.width/2) - (roomWidth/2));
+			} else {
+				roomX = Random.Range(0, map.width - roomWidth + 1);
+			}
+			
+			int roomY = Random.Range(0, map.height - roomHeight + 1);
+			
+			Room r = new Room (roomX, roomY, roomWidth, roomHeight);
+			
+			if(overlappingRooms || !roomCollides(r)) {
+				createRoom (r);
+			}
+		}
+		
+		// Randomly connect the rooms
+		for (int i = 0; i < rooms.Count; i++) {
+			int j = i + Random.Range(1, rooms.Count);
+			j %= rooms.Count;
+			
+			createHallway (rooms [i], rooms [j]);
+		}
+		
+		// Make sure there are no isolated groups of rooms
+		connectAllRooms ();
+		
+		verifyBorderWalls ();
+		
+		if (mirrorMap) {
+			for (int x = 0; x < map.width/2; x++) {
+				for (int y = 0; y < map.height; y++) {
+					map.tiles [map.width - 1 - x, y] = map.tiles [x, y];
+				}
+			}
+			
+			if(!map.isConnectedTiles(1)) {
+				connectMirroredMap ();
+			}
+		}
+		
+		return map;
+	}
 	
+	public bool roomCollides(Room r) {
+		foreach (Room r2 in rooms) {
+			if(r.innerRoomCollidesWith(r2)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private void createRoom(Room r) {
+		for(int x = 0; x < r.width; x++) {
+			for(int y = 0; y < r.height; y++) {
+				if(x == 0 || x == r.width-1 || y == 0 || y == r.height-1) {
+					map.tiles[x + r.x, y + r.y] = 2;
+				} else {
+					map.tiles[x + r.x, y + r.y] = 1;
+				}
+			}
+		}
+		
+		rooms.Add (r);
+	}
+	
+	private void createHallway(Room r1, Room r2) {
+		int dx = (r1.centerX < r2.centerX) ? 1 : -1;
+		int dy = (r1.centerY < r2.centerY) ? 1 : -1;
+		
+		int innerWidth = Random.Range (innerHallwayWidthRange [0], innerHallwayWidthRange [1] + 1);
+		
+		int width1 = -1 * innerWidth / 2;
+		int width2 = innerWidth / 2;
+		if (innerWidth % 2 == 1) {
+			width2 += 1;
+		}
+		
+		for (int y = r1.centerY + width1; y < r1.centerY + width2; y++) {
+			for (int x = r1.centerX; x != r2.centerX + (dx * innerWidth / 2); x += dx) {
+				setHallwayTile(x, y);
+			}
+		}
+		
+		for (int x = r2.centerX + width1; x < r2.centerX + width2; x++) {
+			for (int y = r1.centerY; y != r2.centerY + (dy * innerWidth / 2); y += dy) {
+				setHallwayTile(x, y);
+			}
+		}
+		
+		r1.addConnection (r2);
+		r2.addConnection (r1);
+	}
+	
+	private void setHallwayTile(int x, int y) {
+		if (x > 0 && x < map.width && y > 0 && y < map.height) {
+			map.tiles [x, y] = 1;
+			
+			if (x > 0 && map.tiles [x - 1, y] == 0) {
+				map.tiles [x - 1, y] = 2;
+			}
+			
+			if (x + 1 < map.width && map.tiles [x + 1, y] == 0) {
+				map.tiles [x + 1, y] = 2;
+			}
+			
+			if (y > 0 && map.tiles [x, y - 1] == 0) {
+				map.tiles [x, y - 1] = 2;
+			}
+			
+			if (y + 1 < map.height && map.tiles [x, y + 1] == 0) {
+				map.tiles [x, y + 1] = 2;
+			}
+			
+			if (x > 0 && y > 0 && map.tiles [x - 1, y - 1] == 0) {
+				map.tiles [x - 1, y - 1] = 2;
+			}
+			
+			if (x + 1 < map.width && y > 0 && map.tiles [x + 1, y - 1] == 0) {
+				map.tiles [x + 1, y - 1] = 2;
+			}
+			
+			if (x > 0 && y + 1 < map.height && map.tiles [x - 1, y + 1] == 0) {
+				map.tiles [x - 1, y + 1] = 2;
+			}
+			
+			if (x + 1 < map.width && y + 1 < map.height && map.tiles [x + 1, y + 1] == 0) {
+				map.tiles [x + 1, y + 1] = 2;
+			}
+		}
+	}
+	
+	private void connectAllRooms() {
+		List<List<Room>> connectedRooms = new List<List<Room>>();
+		
+		HashSet<Room> visited = new HashSet<Room> ();
+		
+		for(int i = 0; i < rooms.Count; i++) {
+			if(!visited.Contains(rooms[i])) {
+				connectedRooms.Add(getConnectedRooms(rooms[i], visited));
+			}
+		}
+		
+		if (connectedRooms.Count > 1) {
+			for(int i = 0; i < connectedRooms.Count-1; i++) {
+				Room r1 = connectedRooms[i][0];
+				Room r2 = connectedRooms[i+1][0];
+				
+				createHallway(r1, r2);
+			}
+		}
+	}
+	
+	private List<Room> getConnectedRooms(Room startRoom, HashSet<Room> visited) {
+		List<Room> connectedRooms = new List<Room> ();
+		
+		List<Room> list = new List<Room> ();
+		list.Add (startRoom);
+		
+		while (list.Count > 0) {
+			Room r = list[list.Count-1];
+			list.RemoveAt(list.Count-1);
+			
+			if(!visited.Contains(r)) {
+				visited.Add(r);
+				connectedRooms.Add(r);
+				
+				foreach(Room c in r.connectedTo) {
+					if(!visited.Contains(c)) {
+						list.Add(c);
+					}
+				}
+			}
+		}
+		
+		return connectedRooms;
+	}
+	
+	/** 
+	 * Make sure any hallways against the borders of the map are fully 
+	 * enclosed in wall times (even if that means we have to shave off 
+	 * some tiles from the hallway 
+	 */
+	private void verifyBorderWalls() {
+		for (int x = 0; x < map.width; x++) {
+			setBorderWall(x, 0);
+			setBorderWall(x, map.height-1);
+		}
+		
+		for (int y = 0; y < map.height; y++) {
+			setBorderWall(0, y);
+			setBorderWall(map.width-1, y);
+		}
+	}
+	
+	private void setBorderWall(int x, int y) {
+		if (map.tiles [x, y] == 1) {
+			map.tiles[x, y] = 2;
+		}
+	}
+	
+	private void connectMirroredMap ()
+	{
+		int currentMaxX = -1;
+		Room currentRoom = null;
+		for (int i = 0; i < rooms.Count; i++) {
+			if (rooms [i].maxX > currentMaxX) {
+				currentMaxX = rooms [i].maxX;
+				currentRoom = rooms [i];
+			}
+		}
+		int connectionWidth = map.width - 2 * currentRoom.x;
+		for (int x = 1; x < connectionWidth - 1; x++) {
+			for (int y = 1; y < currentRoom.height - 1; y++) {
+				setHallwayTile (x + currentRoom.x, y + currentRoom.y);
+			}
+		}
 	}
 }
---
+++ ludum-dare-33/Assets/Scripts/Map/Room.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room {
+	public int x;
+	public int y;
+	public int width;
+	public int height;
+
+	public List<Room> connectedTo;
+
+	public int maxX {
+		get 
+		{
+			return x+width; 
+		}
+	}
+
+	
+	public int maxY {
+		get 
+		{
+			return y+height; 
+		}
+	}
+
+	public int centerX {
+		get
+		{
+			return x + width/2;
+		}
+	}
+
+	public int centerY {
+		get
+		{
+			return y + height/2;
+		}
+	}
+
+	public Room(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		connectedTo = new List<Room> ();
+	}
+
+	public bool innerRoomCollidesWith(Room r) {
+		if(maxX-1 <= r.x || x >= r.maxX-1 || maxY-1 <= r.y || y >= r.maxY-1) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public void addConnection(Room r) {
+		if (connectedTo.IndexOf (r) == -1) {
+			connectedTo.Add(r);
+		}
+	}
+
+	public override string ToString()
+	{
+		return "( x: " + this.x + ", y: " + this.y + ", width: " + this.width + ", height: " + this.height + " )";
+	}
+}
