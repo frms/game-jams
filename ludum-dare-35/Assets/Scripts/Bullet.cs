a3d0d73be6ffe95ab11ea35c555d8dfdf488aacc
Anton Pantev <pantevanton@gmail.com>
2016-04-16 00:34:00 -0400

    shoot bullet

--- ludum-dare-35/Assets/Scripts/Bullet.cs
+++ ludum-dare-35/Assets/Scripts/Bullet.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
+    public float speed;
+
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = -transform.up * speed;
 	}
 	
 	// Update is called once per frame
