57c619bacd3fbb784d055e971424eca4aa60886b
Anton Pantev <pantevanton@gmail.com>
2016-04-15 23:50:45 -0400

    added camera/movement for player

--- ludum-dare-35/Assets/unity-movement-ai/Scripts/ThirdPersonUnit.cs
+++ ludum-dare-35/Assets/unity-movement-ai/Scripts/ThirdPersonUnit.cs
@@ -17,28 +17,17 @@ public class ThirdPersonUnit : MonoBehaviour {
     private float vertAxis = 0f;
     private float sideStepDir = 0f;
 
-    private Transform cam;
-
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<MovementAIRigidbody>();
-        cam = GetComponentInChildren<Camera>().transform;
-
-        lookAtChar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            rotateCamera();
-        }
-
         horAxis = Input.GetAxisRaw("Horizontal");
         vertAxis = Input.GetAxisRaw("Vertical");
-        //vertAxis = 1f;
 
         if (Input.GetKey(KeyCode.Q))
         {
@@ -50,29 +39,6 @@ public class ThirdPersonUnit : MonoBehaviour {
         {
             sideStepDir = 0f;
         }
-    }
-
-    private void rotateCamera()
-    {
-        float radius = cam.localPosition.magnitude;
-        float theta = Mathf.Acos(cam.localPosition.y / radius);
-        float phi = Mathf.Atan(cam.localPosition.z / cam.localPosition.x);
-
-        theta += Input.GetAxis("Mouse Y") * Mathf.Deg2Rad;
-
-        Vector3 newPos = new Vector3();
-        newPos.x = -1 * radius * Mathf.Sin(theta) * Mathf.Cos(phi);
-        newPos.y = radius * Mathf.Cos(theta);
-        newPos.z = radius * Mathf.Sin(theta) * Mathf.Sin(phi);
-
-        cam.localPosition = newPos;
-
-        lookAtChar();
-    }
-
-    private void lookAtChar()
-    {
-        cam.LookAt(transform.position + (rb.boundingRadius * Vector3.up));
     }
 
     void FixedUpdate()
