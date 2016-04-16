de702ff611bc3ce38944c101d6067d8d31182132
Anton Pantev <pantevanton@gmail.com>
2016-04-16 14:52:02 -0400

    Beginning to make the first enemy

---
+++ ludum-dare-35/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    public LinePath path;
+
+    private SteeringBasics steeringBasics;
+    private WallAvoidance wallAvoidance;
+    private FollowPath followPath;
+
+    // Use this for initialization
+    void Start()
+    {
+        path.calcDistances();
+
+        steeringBasics = GetComponent<SteeringBasics>();
+        wallAvoidance = GetComponent<WallAvoidance>();
+        followPath = GetComponent<FollowPath>();
+    }
+
+    void FixedUpdate()
+    {
+        if (followPath.isAtEndOfPath(path))
+        {
+            path.reversePath();
+        }
+
+        Vector3 accel = wallAvoidance.getSteering();
+
+        if (accel.magnitude < 0.005f)
+        {
+            accel = followPath.getSteering(path);
+        }
+
+        steeringBasics.steer(accel);
+        steeringBasics.lookWhereYoureGoing();
+
+        path.draw();
+    }
+}
