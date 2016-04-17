90c08523dc5bd539f6eeb64acf7cb51fd8bc1483
Anton Pantev <pantevanton@gmail.com>
2016-04-17 02:11:07 -0400

    missed files last commit

---
+++ ludum-dare-35/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MovementAIRigidbody))]
+public class Player: Health {
+    public Transform bar;
+    public Vector3 barSize = new Vector3(1, 1, 1);
+
+    public float speed = 5;
+
+    public float turnSpeed = 120f;
+
+    private MovementAIRigidbody rb;
+
+    private float horAxis = 0f;
+    private float vertAxis = 0f;
+    private float sideStepDir = 0f;
+
+    public Transform bulletSpawnPoint;
+    public Transform bulletPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<MovementAIRigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horAxis = Input.GetAxisRaw("Horizontal");
+        vertAxis = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            sideStepDir = 1f;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            sideStepDir = -1f;
+        }
+        else
+        {
+            sideStepDir = 0f;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 eulerAngles = bulletPrefab.eulerAngles;
+            eulerAngles.y = transform.eulerAngles.y;
+            Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.Euler(eulerAngles));
+        }
+
+        // Make the  bar scale to the current barProgress
+        bar.localScale = new Vector3(percentHealth * barSize.x, barSize.y, barSize.z);
+    }
+
+    void FixedUpdate()
+    {
+        rotateChar();
+        moveChar();
+    }
+
+    private void moveChar()
+    {
+        Vector3 vel = (transform.right * vertAxis) + (transform.forward * sideStepDir);
+        vel = vel.normalized * speed;
+        rb.velocity = vel;
+    }
+
+    private void rotateChar()
+    {
+        rb.angularVelocity = horAxis * turnSpeed * Mathf.Deg2Rad;
+        // Clear out any x/z orientation
+        rb.rotation = Quaternion.Euler(0, rb.rotation.eulerAngles.y, 0);
+    }
+
+    void OnDestroy()
+    {
+        if (bar != null)
+        {
+            Destroy(bar.gameObject);
+        }
+    }
+}
---
+++ ludum-dare-35/Assets/Scripts/RestartBtn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartBtn : MonoBehaviour {
+
+	public void restart() {
+		SceneManager.LoadScene("Scene1");
+		//GameManager.Instance.sceneIsEnding = true;
+	}
+}
