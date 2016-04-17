3d590b44f889d1b073d6e8bc4504032c8d267639
Anton Pantev <pantevanton@gmail.com>
2016-04-17 00:48:43 -0400

    random colors

--- ludum-dare-35/Assets/Scripts/Wall.cs
+++ ludum-dare-35/Assets/Scripts/Wall.cs
@@ -3,17 +3,34 @@ using System.Collections;
 
 public class Wall : MonoBehaviour {
 
+    public Color[] wallColors;
+    public float percentPinkWalls = 0.5f;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        randomColor();
+
         Fader f = GetComponent<Fader>();
         f.setAlpha(0);
 
-        f.targetAlpha(1, 0.5f + 1.7f*Random.value);
+        f.targetAlpha(1, 0.5f + 1.7f * Random.value);
         //f.targetAlpha(1, 1.5f);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void randomColor()
+    {
+        int colorIndex = 0;
+
+        if (Random.value > percentPinkWalls)
+        {
+            colorIndex = Random.Range(1, wallColors.Length);
+        }
+        GetComponent<Renderer>().material.color = wallColors[colorIndex];
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
