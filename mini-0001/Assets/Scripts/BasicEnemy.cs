d0f7d3903a729a85514746fe182c13cc20663287
Anton Pantev <pantevanton@gmail.com>
2016-05-13 15:13:28 -0400

    Beginning to add a basic enemy

---
+++ mini-0001/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class BasicEnemy : MonoBehaviour {
+
+    private PlatformerCharacter2D m_Character;
+    private bool m_Jump;
+
+
+    void Awake()
+    {
+        m_Character = GetComponent<PlatformerCharacter2D>();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = false;
+        float h = 0;
+
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, crouch, m_Jump);
+        m_Jump = false;
+    }
+}
