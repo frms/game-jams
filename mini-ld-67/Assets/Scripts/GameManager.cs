dab08da87c83bdd38bfcbb029f34225c2a31e07a
Anton Pantev <pantevanton@gmail.com>
2016-05-22 03:13:24 -0400

    spawning basic dots

--- mini-ld-67/Assets/Scripts/GameManager.cs
+++ mini-ld-67/Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ScreenBoundary : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    public GameObject dotPrefab;
+    public int[] numDots;
+
     private Vector3 bottomLeft;
     private Vector3 topRight;
     private Vector3 widthHeight;
@@ -17,6 +19,16 @@ public class ScreenBoundary : MonoBehaviour
         widthHeight = topRight - bottomLeft;
 
         transform.localScale = new Vector3(widthHeight.x, transform.localScale.y, widthHeight.z);
+
+        float num = Random.Range(numDots[0], numDots[1] + 1);
+        for(int i = 0; i < num; i++)
+        {
+            Vector3 pos = new Vector3();
+            pos.x = Random.Range(bottomLeft.x, topRight.x);
+            pos.z = Random.Range(bottomLeft.z, topRight.z);
+
+            Instantiate(dotPrefab, pos, Quaternion.identity);
+        }
     }
 
     void OnTriggerStay(Collider other)
