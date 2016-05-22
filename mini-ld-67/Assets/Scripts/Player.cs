200e10821950e34b3d542c3e3d54b4ecba3dea73
Anton Pantev <pantevanton@gmail.com>
2016-05-22 02:20:35 -0400

    basic player movement

---
+++ mini-ld-67/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float thrust;
+    public float maxSpeed;
+    public float turnSpeed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        float horAxis = Input.GetAxisRaw("Horizontal");
+        rb.angularVelocity = new Vector3(0, horAxis * turnSpeed * Mathf.Deg2Rad, 0);
+
+        float vertAxis = Input.GetAxisRaw("Vertical");
+        rb.AddForce(transform.right * vertAxis * thrust, ForceMode.Acceleration);
+
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
