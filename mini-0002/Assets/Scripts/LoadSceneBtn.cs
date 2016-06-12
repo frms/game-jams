5efff9fdf78c71b15fea00afb99992383d4a8b67
Anton Pantev <pantevanton@gmail.com>
2016-06-12 00:14:59 -0400

    Beginning to add persistence

---
+++ mini-0002/Assets/Scripts/LoadSceneBtn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneBtn : MonoBehaviour
+{
+    public string sceneName;
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
