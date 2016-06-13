8cfd1381d28c7d1f704ba35cb8b880d0619edee5
Anton Pantev <pantevanton@gmail.com>
2016-06-13 14:09:14 -0400

    Starting to move the player.

---
+++ mini-0003/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+
+        Vector2 vel = Vector2.zero;
+        vel.x = xAxis * speed;
+        rb.velocity = vel;
+
+        Debug.Log(xAxis + " " + vel.ToString("F4"));
+	}
+}
