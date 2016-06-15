cb85aed26d79f9e640130f2ed81bcbcdaf3ea06b
Anton Pantev <pantevanton@gmail.com>
2016-06-15 16:48:53 -0400

    in progress lvl 10

---
+++ mini-0003/Assets/Scripts/FinalGoal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FinalGoal : MonoBehaviour
+{
+    public Text winText;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            other.GetComponent<Player>().enabled = false;
+            winText.enabled = true;
+        }
+    }
+}
