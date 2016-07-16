cd912b0d5026e86126f8ea97bf4f7561097e5ea3
Anton Pantev <pantevanton@gmail.com>
2016-07-15 21:41:14 -0400

    Added random names. Made the exp bar and text change as exp changes.

--- mini-0005/js/creature.js
+++ mini-0005/js/creature.js
@@ -8,7 +8,7 @@ class BattleUI {
 		this.healthBar = elem.find('.health-bar');
 		this.healthText = elem.find('.health-text');
 		this.expBar = elem.find('.exp-bar');
-		this.expText = elem.find('.health-text');
+		this.expText = elem.find('.exp-text');
 	}
 
 	setUp(creature) {
@@ -18,18 +18,21 @@ class BattleUI {
 
 	update(creature) {
 		this.level.text(`Lvl ${creature.level}`);
+
 		this.healthText.text(creature.hp);
 		let percent = Math.max(100*creature.hp/creature.maxHp, 0);
 		this.healthBar.css('width', `${percent}%`);
+
+		this.expText.text(`${creature.levelExp}/${creature.levelMaxExp}`);
+		this.expBar.css('width', `${100*creature.levelExp/creature.levelMaxExp}%`);
 	}
 }
 
 class Creature {
 
 	constructor(num, lvl, battleUI) {
-		this.num = num;
-		this.name = num;
-
+		this.base = Creature.list[num];
+		this.name = this.base.name;
 		this.exp = Math.pow(lvl, 3);
 
 		this.battleUI = battleUI;
@@ -40,10 +43,6 @@ class Creature {
 		if(this.battleUI) {
 			this.battleUI.update(this)
 		}
-	}
-
-	get baseStats() {
-		return Creature.list[this.num];
 	}
 
 	get hp() {
@@ -68,7 +67,7 @@ class Creature {
 		this.curExp = val;
 
 		if(startLevel != this.level) {
-			let stats = this.baseStats;
+			let stats = this.base;
 			this.maxHp = stats.hp * this.level;
 			this.dmg = stats.dmg * this.level;
 
@@ -78,8 +77,16 @@ class Creature {
 		}
 	}
 
+	get levelExp() {
+		return Math.trunc(this.exp - Math.pow(this.level, 3));
+	}
+
+	get levelMaxExp() {
+		return Math.trunc(Math.pow(this.level + 1, 3) - Math.pow(this.level, 3));
+	}
+
 	get defeatExp() {
-		let stats = this.baseStats;
+		let stats = this.base;
 		return ((Math.sqrt(stats.hp) + Math.sqrt(stats.dmg)) * this.level) / 6;
 	}
 
@@ -98,70 +105,87 @@ class Creature {
 
 Creature.list = [
 	{
+		"name": "Angela",
 		"hp": "1715.85083",
 		"dmg": "666.5039063"
 	},
 	{
+		"name": "Betty",
 		"hp": "1274.108887",
 		"dmg": "585.9375"
 	},
 	{
+		"name": "Charlotte",
 		"hp": "1705.932617",
 		"dmg": "585.9375"
 	},
 	{
+		"name": "Daisy",
 		"hp": "1025.390625",
 		"dmg": "527.34375"
 	},
 	{
+		"name": "Evie",
 		"hp": "843.8110352",
 		"dmg": "1312.5"
 	},
 	{
+		"name": "Freya",
 		"hp": "769.0429688",
 		"dmg": "153.8085938"
 	},
 	{
+		"name": "Gracie",
 		"hp": "923.9196777",
 		"dmg": "205.078125"
 	},
 	{
+		"name": "Holly",
 		"hp": "1493.835449",
 		"dmg": "0"
 	},
 	{
+		"name": "Isla",
 		"hp": "1319.885254",
 		"dmg": "0"
 	},
 	{
+		"name": "Jessica",
 		"hp": "1243.591309",
 		"dmg": "585.9375"
 	},
 	{
+		"name": "Kaiden",
 		"hp": "794.6777344",
 		"dmg": "791.015625"
 	},
 	{
+		"name": "Logan",
 		"hp": "926.9714355",
 		"dmg": "527.34375"
 	},
 	{
+		"name": "Mia",
 		"hp": "1812.744141",
 		"dmg": "527.34375"
 	},
 	{
+		"name": "Noah",
 		"hp": "1025.390625",
 		"dmg": "527.34375"
 	},
 	{
+		"name": "Olivia",
 		"hp": "1367.950439",
 		"dmg": "546.875"
 	},
 	{
+		"name": "Patrick",
 		"hp": "2563.476563",
 		"dmg": "1171.875"
 	},
 	{
+		"name": "Queenie",
 		"hp": "1094.360352",
 		"dmg": "703.125"
 	}
