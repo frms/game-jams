0246f27fa5b87dd166a29b862d5727f2967d3f40
Anton Pantev <pantevanton@gmail.com>
2016-07-20 16:31:53 -0400

    Showing the line between the beginning and end points.

---
+++ mini-0006/Assets/Scripts/PassThroughTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PassThroughTest : MonoBehaviour
+{
+
+    public Transform start;
+    public Transform end;
+
+    public Transform squarePrefab;
+    public Transform circlePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 disp = end.position - start.position;
+
+        float zRot = Mathf.Atan2(disp.y, disp.x) * Mathf.Rad2Deg;
+
+        Transform line = Instantiate(squarePrefab, start.position + disp / 2f, Quaternion.Euler(0, 0, zRot)) as Transform;
+        Vector3 scale = line.localScale;
+        scale.x = disp.magnitude + 1f;
+        line.localScale = scale;
+
+        SpriteRenderer sr = line.GetComponent<SpriteRenderer>();
+        Color color = sr.color;
+        color.a = 0.35f;
+        sr.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
